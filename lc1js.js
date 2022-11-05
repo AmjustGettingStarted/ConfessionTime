22b3dc6d1ea9406dec47edd73f2d729c1b9ebd05
AmjustGettingStarted <111118246+AmjustGettingStarted@users.noreply.github.com>
2022-11-05 17:14:06 +0530

    Add files via upload

---
+++ lc1js.js
@@ -0,0 +1,29 @@
+/* All Thanks to -Serhii @sakals for his appreciated help */
+console.clear();
+const btn = document.querySelector(".toggle-state");
+btn.click();
+
+function toggleState(key) {
+	const changeStateTo = val =>
+		document.documentElement.style.setProperty(key, val);
+
+	const currentState = getComputedStyle(
+		document.documentElement
+	).getPropertyValue(key);
+
+	currentState === "running"
+		? changeStateTo("paused")
+		: changeStateTo("running");
+}
+
+btn.addEventListener(
+	"click",
+	() => toggleState("--defaultAnimationState"),
+	false
+);
+
+function svolume(){
+    var myaudio=document.getElementById("audio1");
+    myaudio.volume=0.5;
+    // Set half volume;
+}
